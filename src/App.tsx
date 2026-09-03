1b8b1f15c74191e6584747298342344fa7742210
ryanmc1205 <i2yan5@hotmail.com>
2026-09-03 18:41:19 -0500

    Refactor App component to use AppRoutes

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,8 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { HelmetProvider } from "react-helmet-async";
-
-import Index from "./pages/Index";
-import NotFound from "./pages/NotFound";
-import WeddingsPage from "./pages/weddings";
-import CorporatePage from "./pages/corporate";
-import AboutPage from "./pages/about";
-import BlogIndex from './pages/blog-index';
-import BlogArticleLayout from './BlogArticleLayout';
-import FiveQuestionsPage from "./pages/5-questions";
-import IGRedirect from "./pages/IGRedirect";
-import { useAnalytics } from "@/hooks/use-analytics";
-
-// 🔧 This ensures the hook only runs once Router is mounted
-const AnalyticsTracker = () => {
-  useAnalytics();
-  return null;
-};
+import React from "react";
+import { AppRoutes } from "./AppRoutes";
 
 const App = () => {
-  return (
-    <HelmetProvider>
-      <BrowserRouter>
-        <AnalyticsTracker />
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/weddings" element={<WeddingsPage />} />
-          <Route path="/corporate" element={<CorporatePage />} />
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/blog" element={<BlogIndex />} />
-          <Route path="/blog/:slug" element={<BlogArticleLayout />} />
-          <Route path="/5-questions" element={<FiveQuestionsPage />} />
-          <Route path="/ig" element={<IGRedirect />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </HelmetProvider>
-  );
+  return <AppRoutes />;
 };
 
 export default App;
