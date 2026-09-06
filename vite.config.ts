53e7cc55aac35a19764f26ed7691be7154271243
ryanmc1205 <i2yan5@hotmail.com>
2026-09-06 18:14:29 -0500

    Update Vite config for SSR build support
    Refactor Vite config to support SSR builds with conditional settings.

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-export default defineConfig({
+export default defineConfig(({ isSsrBuild }) => ({
   server: {
     host: "::",
     port: 8080,
@@ -19,4 +19,14 @@ export default defineConfig({
   ssr: {
     noExternal: ["react-helmet-async"],
   },
-});
+
+  build: isSsrBuild
+    ? {
+        rollupOptions: {
+          output: {
+            inlineDynamicImports: true,
+          },
+        },
+      }
+    : undefined,
+}));
