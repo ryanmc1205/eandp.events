1cafa1646e994f9e5c9132cccc7fe06d526754c5
ryanmc1205 <i2yan5@hotmail.com>
2026-09-06 18:02:20 -0500

    Modify vite.config to support SSR builds

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-export default defineConfig(() => ({
+export default defineConfig(({ isSsrBuild }) => ({
   server: {
     host: "::",
     port: 8080,
@@ -18,11 +18,13 @@ export default defineConfig(() => ({
 
   build: {
     rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ["react", "react-dom"],
-        },
-      },
+      output: isSsrBuild
+        ? {}
+        : {
+            manualChunks: {
+              vendor: ["react", "react-dom"],
+            },
+          },
     },
   },
 }));
