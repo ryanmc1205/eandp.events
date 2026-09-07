0cf0b26f36b54589552996d637a30ae25b9a581f
ryanmc1205 <i2yan5@hotmail.com>
2026-09-07 11:19:20 -0500

    Add static routes and sitemap excluded routes

---
+++ scripts/site-routes.mjs
@@ -0,0 +1,14 @@
+export const staticRoutes = [
+  "/",
+  "/weddings",
+  "/corporate",
+  "/about",
+  "/blog",
+  "/5-questions",
+  "/privacy-policy",
+  "/unsubscribe",
+];
+
+export const sitemapExcludedRoutes = [
+  "/unsubscribe",
+];
